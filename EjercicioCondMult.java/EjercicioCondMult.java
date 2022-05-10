9a793f7e668dd7d30ab22eea09c37d3023e52dbc
FranklinRamos17 <javier.quispe.r@upeu.edu.pe>
2022-05-10 16:52:39 +0000

    ejerciciomultiple

---
+++ EjercicioCondMult.java/EjercicioCondMult.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class EjercicioCondMult {
+
+    public static void profesor() {
+         System.out.println("La funcion del docente es enseñar");   
+    }
+
+    public static void estudiante() {
+        System.out.println("La funcion del estudiante es aprender");
+    }  
+
+    public void maquinapc() {
+        System.out.println("La funcion de la pc es procesar informacion");
+    }
+
+    public void proyector() {
+        System.out.println("La funcion del proyector es proyectar informacion");    
+    }
+
+    public static void main(String[] args) {
+           EjercicioCondMult objx=new EjercicioCondMult();
+
+           Scanner lt=new Scanner(System.in);  
+           System.out.println("Ingrese el algoritmo que desea probar\n1=profesor"+
+           "\n2=estudiante\n3=maquinapc\n4=proyector");
+           int opcMet=lt.nextInt();
+            switch (opcMet) {
+                case 1:{profesor(); } break;
+                case 2: estudiante(); break;
+                case 3: objx.maquinapc(); break;
+                case 4: objx.proyector(); break;
+
+                default: System.err.println("Opcion no existe"); break;
+            }
+
+    }
+
+}
+
+2
